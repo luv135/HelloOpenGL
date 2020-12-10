a20d016b48caab4ff0e2d7a1321445a8218363e8
luowei <luv135@163.com>
2020-12-10 20:12:33 +0800

    顶点着色器设置顶点位置和颜色

--- HelloOpenGL/Hello.cpp
+++ HelloOpenGL/Hello.cpp
@@ -36,13 +36,26 @@ const char* fragmentShaderSource = "#version 330 core\n"
     "   FragColor = ourColor;\n"
     "}\n\0";
 
+//-------------------------22--------------
+
+const char* vertexShaderSource2 = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 vertexColor;\n"   //定义颜色提供给片段着色器
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   vertexColor = aColor;"
+"}\0";
 
 
 const char* fragmentShaderSource2 = "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec3 vertexColor;\n"    //获取顶点着色器中定义的颜色,名称相同即可
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f);\n"
+    //"   FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(vertexColor,1.0f);\n"
     "}\n\0";
 
 
@@ -124,37 +137,11 @@ int main()
     }
 
 
-
-
-
-
-
-
-
-
-    unsigned int fragmentShader2;
-    fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
-    // 创建片段着色器
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
-    glCompileShader(fragmentShader2);
-
-
-    unsigned int shaderProgram2;
-    // 创建着色器程序
-    shaderProgram2 = glCreateProgram();
-    // 着色器附加到程序
-    glAttachShader(shaderProgram2, vertexShader);
-    glAttachShader(shaderProgram2, fragmentShader2);
-    // 链接
-    glLinkProgram(shaderProgram2);
-
-
     // 链接到程序后,不再需要,删除
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
-    glDeleteShader(fragmentShader2);
 
+    
 
 
     float vertices[] = {
@@ -196,10 +183,41 @@ int main()
     glEnableVertexAttribArray(0);
 
     //-------------2--------------
+    unsigned int vertexShader2;
+    // 创建顶点着色器
+    vertexShader2 = glCreateShader(GL_VERTEX_SHADER);
+    // 关联源码
+    glShaderSource(vertexShader2, 1, &vertexShaderSource2, NULL);
+    // 编译
+    glCompileShader(vertexShader2);
+
+    unsigned int fragmentShader2;
+    fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
+    // 创建片段着色器
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
+    glCompileShader(fragmentShader2);
+
+
+    unsigned int shaderProgram2;
+    // 创建着色器程序
+    shaderProgram2 = glCreateProgram();
+    // 着色器附加到程序
+    glAttachShader(shaderProgram2, vertexShader2);
+    glAttachShader(shaderProgram2, fragmentShader2);
+    // 链接
+    glLinkProgram(shaderProgram2);
+
+
+    glDeleteShader(vertexShader2);
+    glDeleteShader(fragmentShader2);
+
+
     float vertices2[] = {
-        0.5f, 0.5f,0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f
+        // 位置               //颜色
+        0.5f, 0.5f,0.0f,        1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.0f,      0.0f, 0.0f, 1.0f
     };
     
     unsigned int VAO2, VBO2;
@@ -211,8 +229,11 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO2);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
 
 
@@ -233,6 +254,7 @@ int main()
         float timeValue = glfwGetTime();
         float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
         int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        // 当前激活的着色器程序中设置值
         glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
 
